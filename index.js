d175bf9fdbe74760c4219fe87eb6e2090bd4bb11
Marcio Silva <marciosilvadrum@gmail.com>
2022-12-09 21:47:38 -0300

    config file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import userRoute from "./routes/user.routes.js";
+import connect from "./config/db.config.js";
 
 //habilitar o servidor a ter variáveis de ambiente
 dotenv.config();
@@ -8,7 +9,7 @@ dotenv.config();
 //instanciar a variável que vai ficar responsável pelo nosso servidor -> app
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 //configurar o servidor para aceitar enviar e receber arquivos em JSON
 app.use(express.json());
