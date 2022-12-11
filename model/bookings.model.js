71b721ad98b5db06edd1099f30e93d659e1741bf
Marcio Silva <marciosilvadrum@gmail.com>
2022-12-11 16:07:04 -0300

    change schedule ref from Schedule to User

--- model/bookings.model.js
+++ model/bookings.model.js
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 const bookingsSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User" },
   resource: { type: Schema.Types.ObjectId, ref: "Resource" },
-  schedule: { type: Schema.Types.ObjectId, ref: "Schedule" }, // pegar a referência correta (Schedule não é um model, precisa pegar o campo schedule do UserModel)
+  schedule: { type: Schema.Types.ObjectId, ref: "User" }, // pegar a referência correta (Schedule não é um model, precisa pegar o campo schedule do UserModel)
   status: {
     type: String,
     enum: ["Pendente", "Ativo", "Cancelado"], //trocar para disponível, indisponível e cancelado
