29fba3803fa1abc736ad5f61023d5dbdbe7bbba0
Rodrigo Sotolani <rsotolani@gmail.com>
2022-12-12 09:02:27 -0300

    comentário nos Schemas do banco de dados

--- model/bookings.model.js
+++ model/bookings.model.js
@@ -1,15 +1,34 @@
 import { Schema, model } from "mongoose";
 
 const bookingsSchema = new Schema({
+
+  /** referência ao usuário logado que fez a reserv */
   user: {
     type: Schema.Types.ObjectId,
     ref: "User",
   },
-  resource: { type: Schema.Types.ObjectId, ref: "Resource" },
-  schedule: { type: String }, // pegar a referência correta (Schedule não é um model, precisa pegar o campo schedule do UserModel)
+
+  /** referência ao recurso escolhido para ser reservado */
+  resource: { 
+    type: Schema.Types.ObjectId, 
+    ref: "Resource" 
+  },
+
+  /** data e horário agendado  
+   * pegar a referência correta (Schedule não é um model, precisa pegar o campo schedule do UserModel)
+  */
+  schedule: { 
+    type: String 
+  },
+  
+  /** situação da reserva
+   * pendente, quando é aberta e está aguardando aprovação do gestor do recurso
+   * reservado, quando gestor aprovou a reserva,
+   * cancelado, quando o agendamento não é aprovado ou quando o usuário cancela
+   */
   status: {
     type: String,
-    enum: ["Pendente", "Ativo", "Cancelado"], //trocar para disponível, indisponível e cancelado
+    enum: ["Pendente", "Reservado", "Cancelado"], //trocar para disponível, indisponível e cancelado
     default: "Pendente",
   },
 });
--- model/user.model.js
+++ model/user.model.js
@@ -2,16 +2,20 @@ import { Schema, model } from "mongoose";
 
 const userSchema = new Schema(
   {
+    /** nome completo do usuário */
     name: {
       type: String,
       required: true,
     },
+
+    /** identificação interna do usuário, número de matrícula ou registro funcional */
     idNumber: {
-      //matrícula
       type: String,
       unique: true,
       required: true,
     },
+
+    /** email do usuário */
     email: {
       type: String,
       required: true,
@@ -19,18 +23,28 @@ const userSchema = new Schema(
       lowercase: true,
       match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
     },
+
+    /** papéis que o usuário pode ter no sistema */
     role: {
       type: String,
       enum: ["USER", "GESTOR"],
       default: "USER",
     },
-    resources: [{ type: Schema.Types.ObjectId, ref: "Resource" }], //apenas gestor
+
+    /* id dos recursos que o user possui, somente preenchido quando for Gestor */
+    resources: [
+      { type: Schema.Types.ObjectId, 
+        ref: "Resource" }
+      ], 
+
     // passwordHash: { type: String, required: true },
     // profilePic: { type: String }, //só se tiver tempo
     // confirmEmail: { type: Boolean, default: false },
+    
+    /* referência aos agendamentos realizados pelo usuário */
     bookings: [
       { type: Schema.Types.ObjectId, ref: "Bookings" },
-    ] /* pegar apenas o horário agendado */,
+    ] ,
   },
   {
     timestamps: true,
