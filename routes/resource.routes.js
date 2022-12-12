08512a2dc90e13d8bb1fb9e0212fd74e8a455275
Ana Cláudia L Aleixo <analima0608@gmail.com~>
2022-12-12 17:34:28 -0300

    Ana17h30

--- routes/resource.routes.js
+++ routes/resource.routes.js
@@ -42,13 +42,120 @@ resourceRoute.post(
   }
 );
 
-//get all-resources
-resourceRoute.get("/all-resources", async (req, res) => {
-  try {
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json(error.errors);
+//get all-resource
+
+resourceRoute.get(
+  "/my-resource",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const allResource = await ResourceModel.find({
+        user: req.currentUser._id,
+      }).populate("user");
+
+      return res.status(200).json(allResource);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error.errors);
+    }
   }
-});
+);
+
+//update-resource
+
+resourceRoute.put(
+  "/edit/:idResource",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const { idResource } = req.params;
+
+      const updatedResource = await ResourceModel.findOneAndUpdate(
+        { _id: idResource },
+        { ...req.body },
+        { new: true, runValidators: true }
+      );
+
+      await LogModel.create({
+        user: req.currentUser._id,
+        Resource: idResource,
+        status: `O recurso  "${updatedTask.details}" foi atualizado.`,
+      });
+
+      return res.status(200).json(updatedResource);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error.errors);
+    }
+  }
+);
+
+//delete-resource
+
+resourceRoute.delete(
+  "/delete/:idResource",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const { idResource } = req.params;
+
+      //deletei o recurso
+      const deleteResource = await ResourceModel.findByIdAndDelete(idResource);
+
+      //retirei o id do recurso de dentro da minha array recurso
+      await UserModel.findByIdAndUpdate(
+        deletedResource.user,
+        {
+          $pull: {
+            Resources: idResource,
+          },
+        },
+        { new: true, runValidators: true }
+      );
+
+      await LogModel.create({
+        Resource: idResource,
+        user: req.currentUser._id,
+        status: `O recurso "${deletedResource.details}" foi excluída com o status ${deletedResource.status}.`,
+      });
+
+      return res.status(200).json(deletedResource);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error.errors);
+    }
+  }
+);
+
+resourceRoute.put(
+  "/complete/:idResource",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const { idResource } = req.params;
+
+      const resource = await TaskModel.findByIdAndUpdate(
+        idResource,
+        { complete: true, dateFin: Date.now() },
+        { new: true, runValidators: true }
+      );
+
+      await LogModel.create({
+        user: req.currentUser._id,
+        resource: idResource,
+        status: `A edição "${resource.details}" foi concluída!!`,
+      });
+
+      return res.status(200).json(resource);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error.errors);
+    }
+  }
+);
 
 export default resourceRoute;
